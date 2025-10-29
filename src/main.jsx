654117af2c435e087e3bbcf2da01bd640c59e9c5
chandu <chandugit68@gmail.com>
2025-10-29 10:49:16 +0530

    installed an implemented ReactGA

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
+import ReactGA from "react-ga4"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+ReactGA.initialize("G-PJT34JBFN4");
+
+ReactGA.send("pageview");
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
